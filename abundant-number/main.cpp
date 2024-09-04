0be9ff4a924082eefbdb157a797e0e4e70b15f46
StormLight14 <stormpower44@protonmail.com>
2024-09-04 08:07:27 -0600

    add the stuff ive done so far

---
+++ abundant-number/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+  while (true) {
+    std::cout << "Enter a number (-1 to quit): ";
+
+    int num;
+    if (num == -1) {
+      return 0;
+    }
+  }
+  
+}
+
+
---
+++ financial-calculator/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+  std::string name;
+  std::cout << "Enter your name!:\n";
+  std::cin >> name;
+  std::cout << "Received name: " + name + ".\n";
+}
+
+
