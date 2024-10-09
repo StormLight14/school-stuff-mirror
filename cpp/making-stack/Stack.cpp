0a80dd7ede81af733bdd866dd28e0377f9966715
StormLight14 <stormpower44@protonmail.com>
2024-10-09 09:05:23 -0600

    add linked list implementation in making-stack

---
+++ cpp/making-stack/Stack.cpp
@@ -0,0 +1,56 @@
+#include "Stack.h"
+#include <iostream>
+
+using std::endl, std::ostream;
+
+Stack::Stack() {
+  top = nullptr;
+}
+
+Stack::~Stack() {
+  // delete all of the stack!
+  while (!isEmpty()) {
+    pop();
+  }
+}
+
+void Stack::push(string value) {
+  Node *toAdd = new Node;
+  toAdd->value = value;
+  
+  // stack is empty
+  if (isEmpty()) {
+    top = toAdd; // top points to new Node
+    top->next = nullptr;
+
+    // anything that isn't the first Node
+  } else {
+    toAdd->next = top;
+    top = toAdd;
+  }
+}
+
+bool Stack::isEmpty() {
+  return top == nullptr;
+}
+
+string Stack::getTop() {
+  return top->value;
+}
+
+void Stack::pop() {
+  if (!isEmpty()) {
+    Node *temp = top;
+    top = top->next;
+    delete temp;
+  }
+}
+
+ostream& operator << (ostream &out, const Stack &stack) {
+  Node *current = stack.top;
+  while (current != nullptr) {
+    out << current->value << " ";
+    current = current->next;
+  }
+  return out;
+}
---
+++ cpp/making-stack/Stack.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+
+using std::string, std::ostream;
+
+struct Node {
+  std::string value;
+  Node *next;
+};
+
+class Stack {
+  public:
+    Stack();
+    ~Stack();
+    // add a value to the stack
+    void push(string value);
+    // remove the top value from the stack
+    void pop();
+    // get top value
+    string getTop();
+    // returns true if stack is empty
+    bool isEmpty();
+    friend ostream &operator << (ostream &, const Stack&);
+  private:
+    Node *top;
+};
