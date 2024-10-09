3a349dfa8336e08d7a73fd270a331ac2c168de38
StormLight14 <stormpower44@protonmail.com>
2024-10-09 09:15:08 -0600

    fix Makefile; add main.cpp test of stack

--- cpp/making-stack/main.cpp
+++ cpp/making-stack/main.cpp
@@ -1,3 +1,21 @@
+#include "Stack.h"
+#include <iostream>
+
+using std::cout, std::endl;
+
 int main() {
+  cout << "Stack implemented with linked list!" << endl;
+  Stack stack;
+  stack.push("Lasagna");
+  stack.push("Burger");
+  stack.push("Ice Cream");
+  stack.push("Watermelon");
+
+  cout << stack << endl;
+  stack.pop();
+  stack.pop();
+  stack.pop();
+  cout << stack << endl;
+
   return 0;
 }
