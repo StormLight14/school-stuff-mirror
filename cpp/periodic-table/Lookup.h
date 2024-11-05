e7eb4b328de9cb28b0e547ed70c8237347fb6281
StormLight14 <stormpower44@protonmail.com>
2024-11-05 11:15:52 -0700

    add atomic number lookup

--- cpp/periodic-table/Lookup.h
+++ cpp/periodic-table/Lookup.h
@@ -2,8 +2,22 @@
 #include <vector>
 #include <fstream>
 #include <stdexcept>
+#include <sstream>
+#include <iostream>
 
 using std::string, std::vector;
+
+std::vector<std::string> splitStringByComma(const std::string& str) {
+    std::vector<std::string> result;
+    std::stringstream ss(str);
+    std::string item;
+
+    while (std::getline(ss, item, ',')) {
+        result.push_back(item);
+    }
+
+    return result;
+}
 
 struct Element {
   string atomicNumber;
@@ -12,15 +26,21 @@ struct Element {
   string atomicMass;
 };
 
+std::ostream& operator<<(std::ostream& out, const Element& e) {
+  out << "Number: " << e.atomicNumber << "\nName: " << e.name << "\nSymbol: " << e.symbol << "\nMass: " << e.atomicMass;
+  return out;
+}
+
 class Lookup {
   public:
     void setElements();
+    vector<Element> getElements();
   private:
    vector<Element> elements; 
 };
 
-void setElements() {
-  std::ifstream fin("elements.csv", std::ios::in);
+void Lookup::setElements() {
+  std::ifstream fin("elements.csv", std::ios::in); // elements.csv with read
   if (!fin.good()) {
     throw std::runtime_error("error reading elements.csv");
     return;
@@ -30,7 +50,16 @@ void setElements() {
     string line;
     while (std::getline(fin, line)) {
       Element newElement;
-      
+      vector<string> split = splitStringByComma(line);
+      newElement.atomicNumber = split[0];
+      newElement.name = split[1];
+      newElement.symbol = split[2];
+      newElement.atomicMass = split[3];
+      elements.push_back(newElement);
     }
   }
+}
+
+vector<Element> Lookup::getElements() {
+  return elements;
 }
--- cpp/periodic-table/main.cpp
+++ cpp/periodic-table/main.cpp
@@ -1,3 +1,26 @@
+#include "Lookup.h"
+
+using std::cout, std::cin, std::endl;
+
 int main() {
+  Lookup lookup;
+  lookup.setElements();
+
+  cout << "1. Search by Atomic Number\n2. Search by symbol\n3. Quit" << endl;
+  int input;
+  cin >> input;
+  if (input == 1) {
+    cout << "Enter the atomic number (1-118): ";
+    int atomicNumber;
+    cin >> atomicNumber;
+    if (atomicNumber >= 1 && atomicNumber <= 118) {
+      cout << "\n" << lookup.getElements()[atomicNumber - 1] << endl;
+    } else {
+      cout << "Enter a number from 1-118.";
+    }
+  } else if (input == 2) {
+
+  }
+
   return 0;
 }
