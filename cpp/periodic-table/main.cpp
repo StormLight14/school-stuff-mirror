a2f8d39a4161e20daa51cbb65995587fc4860143
StormLight14 <stormpower44@protonmail.com>
2024-11-05 11:26:31 -0700

    add element symbol lookup

--- cpp/periodic-table/main.cpp
+++ cpp/periodic-table/main.cpp
@@ -1,25 +1,38 @@
 #include "Lookup.h"
 
-using std::cout, std::cin, std::endl;
+using std::cout, std::cin, std::endl, std::string;
 
 int main() {
   Lookup lookup;
   lookup.setElements();
 
-  cout << "1. Search by Atomic Number\n2. Search by symbol\n3. Quit" << endl;
-  int input;
-  cin >> input;
-  if (input == 1) {
-    cout << "Enter the atomic number (1-118): ";
-    int atomicNumber;
-    cin >> atomicNumber;
-    if (atomicNumber >= 1 && atomicNumber <= 118) {
-      cout << "\n" << lookup.getElements()[atomicNumber - 1] << endl;
-    } else {
-      cout << "Enter a number from 1-118.";
-    }
-  } else if (input == 2) {
+  while (true) {
+    cout << "1. Search by Atomic Number\n2. Search by symbol\n3. Quit" << endl;
+    int input;
+    cin >> input;
 
+    if (input == 1) {
+      cout << "Enter the atomic number (1-118): ";
+      int atomicNumber;
+      cin >> atomicNumber;
+
+      if (atomicNumber >= 1 && atomicNumber <= 118) {
+        cout << "\n" << lookup.getElements()[atomicNumber - 1] << endl;
+      } else {
+        cout << "Enter a number from 1-118.";
+      }
+    } else if (input == 2) {
+        cout << "Enter the element's symbol: ";
+        string symbol;
+        cin >> symbol;
+        for (int i = 0; i < 118; i++) {
+          if (lookup.getElements()[i].symbol == symbol) {
+            cout << "\n" << lookup.getElements()[i];
+          }
+        }
+    } else {
+      break;
+    }
   }
 
   return 0;
