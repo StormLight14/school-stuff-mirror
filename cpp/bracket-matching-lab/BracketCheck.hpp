0efb13fb842639b61f96c4de9c2133997606b685
StormLight14 <100660727+StormLight14@users.noreply.github.com>
2025-02-05 15:07:01 +0000

    bracket check worky now let's gooo

--- cpp/bracket-matching-lab/BracketCheck.hpp
+++ cpp/bracket-matching-lab/BracketCheck.hpp
@@ -1,23 +1,24 @@
 #pragma once
 #include <iostream>
+#include <stack>
 
 class BracketCheck {
   public:
     BracketCheck(std::string exp);
-    int numBrackets(); // pairs of brackets
-    bool isBalanced();
+    int numBrackets() const; // pairs of brackets
+    bool isBalanced() const;
     friend std::ostream& operator<<(std::ostream& out, const BracketCheck& bc);
   private:
     std::string exp;
 };
 
 BracketCheck::BracketCheck(std::string exp) {
-  exp = exp;
+  this->exp = exp;
 }
 
-int BracketCheck::numBrackets() {
+int BracketCheck::numBrackets() const {
   int num = 0;
-  for (char& c : exp) {
+  for (char c : exp) {
     if (c == '(' || c == '{' || c == '[') {
       num += 1;
     }
@@ -26,12 +27,44 @@ int BracketCheck::numBrackets() {
   return num;
 }
 
-bool BracketCheck::isBalanced() {
+bool BracketCheck::isBalanced() const {
+  std::stack<char> bracketStack;
+  for (char c : exp) {
+    if (c == '(' || c == '[' || c == '{') {
+      bracketStack.push(c);
+    }
 
+    else if (c == ')') {
+      if (bracketStack.empty() || bracketStack.top() != '(') {
+        return false;
+      }
+
+      bracketStack.pop();
+    }
+
+    else if (c == '}') {
+      if (bracketStack.empty() || bracketStack.top() != '{') {
+        return false;
+      }
+
+      bracketStack.pop();
+    }
+
+    else if (c == ']') {
+      if (bracketStack.empty() || bracketStack.top() != '[') {
+        return false;
+      }
+      bracketStack.pop();
+    }
+  }
+
+  return bracketStack.empty();
 }
 
 std::ostream& operator<<(std::ostream& out, const BracketCheck& bc) {
-  out << "";
+  out << "expression: " << bc.exp << "\n";
+  out << "balanced: " << bc.isBalanced() << "\n";
+  out << "bracket pairs: " << bc.numBrackets() << "\n";
   return out;
 }
 
